5aaabda172d14513c60bd4e59d75dd11aba83592
Lautaro Jovanovics <jovanovicslautaro@gmail.com>
2021-03-24 23:38:41 -0300

    Commit añadiendo funcionalidad js para subir los productos por medio de un array de objetos

--- js/index.js
+++ js/index.js
@@ -1,52 +1,72 @@
 let productos = [
     {
-        nombre: "remera",
-        color: "verde",
-        precioU: 1000,
-        precioM: 900,
+        nombre: "Set Rayo de Escarcha",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/1024_accesorios_abracadabra-juguetes-dados-transp-_agus_1431-6716d8a0561d2dbb7e15936346939925-640-0.jpg",
+        color: "Verde",
+        precioU: 650,
+        precioM: 600,
         stock: true
     },
     {
-        nombre: "short",
-        color: "verde",
-        precioU: 1200,
-        precioM: 1000,
+        nombre: "Set Bola de Fuego",
+        color: "Rojo",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/1024_accesorios_abracadabra-juguetes-dados-transp-rojo_1451-49637647233999189d15936346940040-640-0.jpg",
+        precioU: 675,
+        precioM: 625,
         stock: true
     },
     {
-        nombre: "hoddie",
-        color: "verde",
-        precioU: 1700,
+        nombre: "Set Lluvia de Diamantes",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/1024_accesorios_abracadabra-juguetes-dados-transp-_turquesa_4421-175206094fc4d4073715936346940260-640-0.jpg",
+        color: "Celeste",
+        precioU: 640,
         precioM: 1400,
         stock: true
     },
     {
-        nombre: "medias",
-        color: "verde",
-        precioU: 600,
-        precioM: 500,
+        nombre: "Set Nigromancia Oscura",
+        img: "https://chollosrol.es/wp-content/uploads/2020/01/10-d-10-negros.jpg",
+        color: "Negro",
+        precioU: 690,
+        precioM: 670,
         stock: true
     },
     {
-        nombre: "gorra",
-        color: "verde",
-        precioU: 400,
-        precioM: 350,
+        nombre: "Set Hidromiel",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/1024_accesorios_abracadabra-juguetes-dados-transp-_marron_9421-15f574c3acd1fa44ae15936346938841-1024-1024.jpg",
+        color: "Marron",
+        precioU: 590,
+        precioM: 580,
         stock: true
     },
     {
-        nombre: "zapatillas",
-        color: "rojo",
-        precioU: 1100,
-        precioM: 900,
+        nombre: "Set Oro Silvano",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/1024_accesorios_abracadabra-juguetes-dados-transp-_verde_1421-37e4068ff32b482e7c15936346940616-640-0.jpg",
+        color: "Verdes",
+        precioU: 665,
+        precioM: 650,
         stock: true
     }
 ]
 
-var iva = numero => numero * 1.21;
+for (const producto of productos) {
+    let row = document.querySelector("#productos")
+    let item = document.createElement("div");
+    item.classList.add("card", "col-lg-4", "col-md-6", "col-sm-12")
+    row.appendChild(item)
+    item.innerHTML = `
+                            <img src="${producto.img}">
+                            <h5 class="card-title">${producto.nombre}</h5>
+                            <p class="card-text">$${producto.precioU}</p>
+                            <a href="#" class="btn btn-primary col-6 align-self-end">Agregar al carrito</a><br>
+                            `;
+}
+
+
+/* var iva = numero => numero * 1.21;
 
 do {
-    //var nombre = prompt("¿Qué producto quiere adquirir?");
+    var nombre = prompt("¿Qué producto quiere adquirir?");
     nombre = nombre.toLowerCase();
 }while(nombre =="" || nombre == 0)
 
@@ -67,7 +87,7 @@ if(precioUnitario == undefined || precioMayorista == undefined) {
     alert(`El producto ${nombre} no se encuentra en stock o no está disponible`);
 }else {
     do {
-        //var unidades = Number(prompt("¿Cuantas unidaes quiere comprar?"));
+        var unidades = Number(prompt("¿Cuantas unidaes quiere comprar?"));
     }while(unidades =="" || unidades == 0)
     
     function compra(cantidad, precioU, precioM) {
@@ -87,3 +107,4 @@ if(precioUnitario == undefined || precioMayorista == undefined) {
 
 compra(unidades, precioUnitario, precioMayorista);
 display(unidades, nombre, precio);
+ */
